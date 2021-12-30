7642dffeb88c39c085bba52725bc67cc8f8f093d
MrMerin <32649182+meilin520@users.noreply.github.com>
2021-12-30 15:51:54 +0800

    vue2+typescript移动端示例

---
+++ src/components/layout/TheFooter.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="the-footer">
+        <div class="tabbar">
+            <div class="tabbar-item">{{item}}</div>    
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class TheFooter extends Vue {
+  @Prop() private text!: string;
+}
+</script>
+<style scoped>
+    .the-footer {
+        padding: 15px;
+        text-align: center;
+        font-size: 14px;
+        font-family: Arial, Helvetica, sans-serif;
+        color: #dddddd;
+    }
+</style>
---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="the-header">
+        {{text}}
+    </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class TheHeader extends Vue {
+  @Prop() private text!: string;
+}
+</script>
+<style scoped>
+    .the-header {
+        min-height: 60px;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        padding: 0 15px;
+        text-align: center;
+        font-size: 16px;
+        font-family: Arial, Helvetica, sans-serif;
+        color: #eeeeee;
+        background: #3377ff;
+    }
+</style>
